4b70353fc9f32648d7f3f4fe6daca2212aa11af2
Carlos Eduardo <carlos.eduardo.wzk@gmail.com>
2017-11-23 19:50:14 -0200

    bbb

---
+++ src/main/java/Controle/BookDAO.java
@@ -0,0 +1,17 @@
+package Controle;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import model.Livro;
+
+public class BookDAO {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+		public void save(Livro product) {
+     	manager.persist(product);
+	}
+	
+}
