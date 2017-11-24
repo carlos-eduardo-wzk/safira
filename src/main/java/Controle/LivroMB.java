11cc7f4811a744ee319fe41acfadafd2eb146fe5
Carlos Eduardo <carlos.eduardo.wzk@gmail.com>
2017-11-24 19:07:56 -0200

    ggg

--- src/main/java/Controle/LivroMB.java
+++ src/main/java/Controle/LivroMB.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 import model.Livro;
 
@@ -20,7 +21,7 @@ public class LivroMB {
 	private BookDAO bookDAO;
 	
 	
-
+    @Transactional
 	public void save() {
 		System.out.println("salvar libro " + getProduct().getTitle());
 		bookDAO.save(product);
