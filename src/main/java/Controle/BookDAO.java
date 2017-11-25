0874bdd96d3616ac01454f1966970edd71a9194a
Carlos Eduardo <carlos.eduardo.wzk@gmail.com>
2017-11-25 10:04:11 -0200

    iiiiiii

--- src/main/java/Controle/BookDAO.java
+++ src/main/java/Controle/BookDAO.java
@@ -1,15 +1,18 @@
 package Controle;
 
+import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import model.Livro;
 
+@Stateful
 public class BookDAO {
 
 	@PersistenceContext(unitName = "safiraPU")
 	private EntityManager manager;
+	
 
 	@Transactional
 	public void salva(Livro livro) {
